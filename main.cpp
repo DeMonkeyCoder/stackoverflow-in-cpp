4f44858a374e578778bc3edb81f304c7a674fc90
Navid Far <mojtaba.farokhi99@gmail.com>
2018-10-28 22:05:40 +0330

    exception improved & pass hashing added

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <functional>
+#include <sstream>
 using namespace std;
 
 /**
@@ -17,7 +19,16 @@ enum UserType{
 };
 
 
-class UserAlreadyExistsException:public exception{}; //TODO: Give exceptions a better structure. search google (optional)
+class UserAlreadyExistsException:public exception{
+    const char* message;
+public:
+    UserAlreadyExistsException (char* message){    
+        this->message = message;
+    } 
+    const char* what() const throw(){
+        return message;
+    }
+}; 
 
 class AbstractUser{ // User structure
 public:
@@ -31,20 +42,37 @@ protected:
 
 
 class User : public AbstractUser{
+    hash<string> pass_hash;
 public:
     // string username;
 
     User(string username, string password, UserType type){
         this->username = username;
-        this->password = password;
+        set_password(password);
         this->type = type;
     }
-
+    int check_password(string password){
+        hash<string> pass_hash_c;
+        long long check = pass_hash(password);
+        string a;
+        stringstream out;
+        out << check;
+        a = out.str();
+        return (this->password == a);
+    }
+    void set_password(string password){
+        long long ps = pass_hash(password);
+        string a;
+        stringstream out;
+        out << ps;
+        a = out.str();
+        this->password = a;
+    }
     bool authenticate(string username, string password){
         int ath1 = 0;
         int ath2 = 0;
         if (this->username == username){ath1++;}
-        if (this->password == password){ath2++;}
+        if (check_password(password)){ath2++;}
         if (ath1 == 1 && ath2 == 1){return 1;}
         if (ath1 == 1 && ath2 == 0){throw 2;}  // we have username but the pass is incorrect  
         if (ath1 == 0 && ath2 == 0){return 0;} 
@@ -63,7 +91,8 @@ public:
     static void signup(vector<AbstractUser*> *users, string username, string password){
         for(auto user = users->begin(); user != users->end(); user++) {
             if ((*user)->username == username) {
-                UserAlreadyExistsException ex;
+                char* message="Username already exists";
+                UserAlreadyExistsException ex(message); 
                 throw ex;
             }
         }
@@ -107,13 +136,11 @@ int main(){
     MenuState menuState = MenuState::START;
 
     char choice;
-    cout << "Welcome!" << endl;
 
     while(menuState != MenuState::END){
         switch (menuState){
             case MenuState::START: {
-
-                cout << "1. login\n2. signup\ne. exit\n";
+                cout << "\n1. login\n2. signup\ne. exit\n";
                 cin >> choice;
                 switch(choice) {
                     case '1': {
