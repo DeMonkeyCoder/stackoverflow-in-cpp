b27b8dcf850f17919e285795d79b5d1d8e42c1cd
Mirana <Mini_Mirana@yahoo.com>
2018-10-30 17:00:20 +0330

    make login, sigup, and exit

--- main.cpp
+++ main.cpp
@@ -1,174 +1,250 @@
 #include <iostream>
 #include <vector>
+#include<string>
+#include<exception>
+#include<Windows.h>
 using namespace std;
 
-/**
- * In the name of God
- * Homework 2
- * TODO: Complete this code using given TODO comments :). then remove TODOs
- * feel free to edit code as you want and make it better
- * Any questions? ask @devcom_support on telegram
- * Good luck!
- **/
-
-enum UserType{
-    ADMIN,
-    MEMBER
+enum UserType {
+	ADMIN,
+	MEMBER
 };
 
 
-class UserAlreadyExistsException{}; //TODO: Give exceptions a better structure. search google (optional)
-
-class AbstractUser{ // User structure
+class UserAlreadyExistsException : public exception {
 public:
-    virtual bool authenticate(string username, string password) = 0;
-    virtual bool deleteAccount(vector<AbstractUser*> *users) = 0; //TODO: 1. implement this in User class. (You can't compile code and create instance of User until then). DON'T TOUCH ABSTRACT USER!
-    string username;
+	const char * what() const throw() {
+		return "Error: user already exists\n";
+	}
+};
+
+class UnmatchedUserException : public exception {
+public:
+	const char * what() const throw() {
+		return "Error: unmatched user\n";
+	}
+};
+
+class UnmatchedPasswordException : public exception {
+public:
+	const char * what() const throw() {
+		return "Error: unmatched password\n";
+	}
+};
+
+class UnmatchedUserOrPasswordException : public exception {
+public:
+	const char * what() const throw() {
+		return "Error: unmatched user or password\n";
+	}
+};
+
+class AbstractUser { // User structure
+public:
+	virtual bool authenticate(string username, string password) = 0;
+	virtual bool deleteAccount(vector<AbstractUser*> *users) = 0;
+	string username;
 protected:
-    string password;
-    UserType type;
+	string password;
+	UserType type;
 };
 
 
-class User : public AbstractUser{
+class User : public AbstractUser {
 public:
 
-    User(string username, string password, UserType type){
-        this->username = username;
-        this->password = password;
-        this->type = type;
-    }
+	User(string username, string password, UserType type) {
+		this->username = username;
+		this->password = password;
+		this->type = type;
+	}
 
-    bool authenticate(string username, string password){
-        return this->username == username && this->password == password;
-    }
+	bool authenticate(string username, string password) {
+		return this->username == username && this->password == password;
+	}
 
-    static User* login(vector<AbstractUser*> *users, string username, string password){ //TODO: 2. handle user login errors with exceptions
-        for(auto user = users->begin(); user != users->end(); user++){
-            if((*user)->authenticate(username, password)){
-                return (User*) *user;
-            }
-        }
-        return nullptr;
-    }
+	static User* login(vector<AbstractUser*> *users, string username, string password) {
+		for (auto user = users->begin(); user != users->end(); user++)
+			if ((*user)->authenticate(username, password))
+				return (User*)*user;
+		UnmatchedUserOrPasswordException ex;
+		throw ex;
+		/* return nullptr; */
+	}
 
-    static void signup(vector<AbstractUser*> *users, string username, string password){
+	static void signup(vector<AbstractUser*> *users, string username, string password) {
 
-        //Check if user with that username exists and throw UserAlreadyExistsException in that case
-        for(auto user = users->begin(); user != users->end(); user++) { //TODO: 3. this doesn't work. fix it!!
-            if ((*user)->username == username) {
-                UserAlreadyExistsException ex;
-                throw ex;
-            }
-        }
+		// Check if user with that username exists and throw UserAlreadyExistsException in that case
+		for (auto user = users->begin(); user != users->end(); user++) {
+			if ((*user)->username == username) {
+				UserAlreadyExistsException ex;
+				throw ex;
+			}
+		}
 
-        //Create user and add it to vector
-        users->push_back(new User(username, password, UserType::MEMBER));
-    }
+		// Create user and add it to vector
+		users->push_back(new User(username, password, UserType::MEMBER));
+	}
 
-    string username;
+	bool deleteAccount(vector<AbstractUser*> *users) {
+		string user, pass;
+		cout << "Please enter your username: ";
+		cin >> user;
+		if (username != user) {
+			UnmatchedUserException ex;
+			throw ex;
+		}
+		cout << "Please enter your password: ";
+		HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+		DWORD mode = 0;
+		GetConsoleMode(hStdin, &mode);
+		SetConsoleMode(hStdin, mode & (~ENABLE_ECHO_INPUT));
+		cin >> pass;
+		SetConsoleMode(hStdin, mode);
+		if (password != pass) {
+			UnmatchedPasswordException ex;
+			throw ex;
+		}
+		for (auto user = users->begin(); user != users->end(); user++)
+			if ((*user)->authenticate(username, password)) {
+				users->erase(user);
+				return 1;
+			}
+	}
+
+	/* string username; */
 };
 
-enum MenuState{
-    START,
-    LOGGED_IN,
-    END
+enum MenuState {
+	START,
+	LOGGED_IN,
+	END
 };
 
-class AppDatabase { //Just holds runtime data. doesn't save anything
+class AppDatabase { // Just holds runtime data. doesn't save anything
 public:
-    vector<AbstractUser *> appUsers;
+	vector<AbstractUser *> appUsers;
 
-    AppDatabase() { //Load initial data
-        appUsers.push_back(new User("admin",
-                                    "admin" /* password is unsafe! for test only */,
-                                    UserType::ADMIN)
-        );
-    }
+	AppDatabase() { // Load initial data
+		appUsers.push_back(new User("admin",
+			"admin" /* password is unsafe! for test only */,
+			UserType::ADMIN)
+		);
+	}
 };
 
-int main(){
-    User * loggedInUser = nullptr;
-    AppDatabase appDatabase;
-    MenuState menuState = MenuState::START;
+int main() {
+	User * loggedInUser = nullptr;
+	AppDatabase appDatabase;
+	MenuState menuState = MenuState::START;
 
-    char choice;
-    cout << "Welcome!" << endl;
+	char choice;
+	cout << "Welcome!" << endl;
 
-    while(menuState != MenuState::END){
-        switch (menuState){
-            case MenuState::START: {
+	while (menuState != MenuState::END) {
+		switch (menuState) {
+		case MenuState::START: {
 
-                cout << "1. login\n2. signup\ne. exit\n";
-                cin >> choice;
-                switch(choice) {
-                    case '1': {
-                        string username, password;
-                        cout << "Enter Username" << endl;
-                        cin >> username;
-                        cout << "Enter Password" << endl;
-                        cin >> password;
-                        loggedInUser = User::login(&appDatabase.appUsers, username, password);
-                        if (loggedInUser == nullptr) {
-                            cout << "couldn't login with given credentials.";
-                        } else {
-                            menuState = MenuState::LOGGED_IN;
-                        }
-                        break;
-                    }
-                    case '2': {
-                        string username, password;
-                        cout << "Enter Username" << endl;
-                        cin >> username;
-                        cout << "Enter Password" << endl;
-                        cin >> password;
-                        try{
-                            User::signup(&appDatabase.appUsers, username, password);
-                        } catch (UserAlreadyExistsException e) {
-                            cout << "Error: username already exists";
-                        }
-                        break;
-                    }
-                    case 'e': {
-                        menuState = MenuState::END;
-                        break;
-                    }
-                    default: {
-                        cout << "Unknown Input" << endl;
-                    }
-                }
-                break;
-            }
-            case MenuState::LOGGED_IN: {
-                cout << "d.delete account\nl. logout\ne. exit\n";
-                cin >> choice;
-                switch(choice) {
-                    case 'd': {
-                        loggedInUser->deleteAccount(&appDatabase.appUsers);
-                        cout << "Account successfully deleted";
-                        loggedInUser = nullptr;
-                        menuState = MenuState::START;
-                        break;
-                    }
-                    case 'l': {
-                        loggedInUser = nullptr;
-                        menuState = MenuState::START;
-                        break;
-                    }
-                    case 'e': {
-                        menuState = MenuState::END;
-                        break;
-                    }
-                    default: {
-                        cout << "Unknown Input" << endl;
-                    }
-                }
-                break;
-            }
-        }
-    }
+			cout << "1. login\n2. signup\ne. exit\n";
+			cin >> choice;
+			switch (choice) {
+			case '1': {
+				string username, password;
+				cout << "Enter Username" << endl;
+				cin >> username;
+				cout << "Enter Password" << endl;
+				HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+				DWORD mode = 0;
+				GetConsoleMode(hStdin, &mode);
+				SetConsoleMode(hStdin, mode & (~ENABLE_ECHO_INPUT));
+				cin >> password;
+				SetConsoleMode(hStdin, mode);
+				try {
+					loggedInUser = User::login(&appDatabase.appUsers, username, password);
+					menuState = MenuState::LOGGED_IN;
+				}
+				catch (UnmatchedUserOrPasswordException &e) {
+					cout << e.what();
+				}
+				break;
+				/*
+				if (loggedInUser == nullptr) { ---> i changed here
+				cout << "couldn't login with given credentials.";
+				}
+				else {
+				menuState = MenuState::LOGGED_IN;
+				}
+				break;
+				*/
+			}
+			case '2': {
+				string username, password;
+				cout << "Enter Username" << endl;
+				cin >> username;
+				cout << "Enter Password" << endl;
+				HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+				DWORD mode = 0;
+				GetConsoleMode(hStdin, &mode);
+				SetConsoleMode(hStdin, mode & (~ENABLE_ECHO_INPUT));
+				cin >> password;
+				SetConsoleMode(hStdin, mode);
+				try {
+					User::signup(&appDatabase.appUsers, username, password);
+				}
+				catch (UserAlreadyExistsException &e) {
+					/* cout << "Error: username already exists"; */
+					cout << e.what();
+				}
+				break;
+			}
+			case 'e': {
+				menuState = MenuState::END;
+				break;
+			}
+			default: {
+				cout << "Unknown Input" << endl;
+			}
+			}
+			break;
+		}
+		case MenuState::LOGGED_IN: {
+			cout << "d. delete account\nl. logout\ne. exit\n";
+			cin >> choice;
+			switch (choice) {
+			case 'd': {
+				try {
+					loggedInUser->deleteAccount(&appDatabase.appUsers);
+					cout << "Account successfully deleted" << endl;
+					loggedInUser = nullptr;
+					menuState = MenuState::START;
+				}
+				catch (UnmatchedUserException &e) {
+					cout << e.what();
+				}
+				catch (UnmatchedPasswordException &e) {
+					cout << e.what();
+				}
+				break;
+			}
+			case 'l': {
+				loggedInUser = nullptr;
+				menuState = MenuState::START;
+				break;
+			}
+			case 'e': {
+				menuState = MenuState::END;
+				break;
+			}
+			default: {
+				cout << "Unknown Input" << endl;
+			}
+			}
+			break;
+		}
+		}
+	}
 
-    return 0;
+	return 0;
 
 }
 
