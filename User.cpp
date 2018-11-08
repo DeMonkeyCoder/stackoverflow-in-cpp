a89acf8ba9b0f5ea1441ec7841b54a515fd405fe
spsina <snparvizi75@gmail.com>
2018-11-08 12:32:45 +0330

    User Classed interface and implemantation seperated

--- User.cpp
+++ User.cpp
@@ -2,4 +2,67 @@
 // Created by spsina on 11/8/18.
 //
 
+#include <sstream>
 #include "User.h"
+#include "Exceptions.h"
+using namespace Exceptions;
+
+User::User(string username, string password, UserType type){
+    lower(username);
+    this->username = username;
+    set_password(password);
+    this->type = type;
+}
+void User::set_password(string password){
+    long long ps = pass_hash(password);
+    string a;
+    stringstream out;
+    out << ps;
+    a = out.str();
+    this->password = a;
+}
+bool User::check_password(string password){
+    hash<string> pass_hash_c;
+    long long check = pass_hash(password);
+    string a;
+    stringstream out;
+    out << check;
+    a = out.str();
+    return (this->password == a);
+}
+bool User::authenticate(string username, string password){
+    lower(username);
+    return this->username == username and check_password(password);
+}
+void User::deleteAccount(vector<AbstractUser*> *users){
+    if (this->type == UserType::ADMIN) {
+        DeleteAdminException ex;
+        throw ex;
+    }
+
+    for (auto user = users->begin(); user != users->end();user++){
+        if ((*user)->username == this->username) {
+            users->erase(user);
+            break;
+        }
+    }
+}
+User* User::login(vector<AbstractUser*> *users, string username, string password){
+    for(auto user = users->begin(); user != users->end(); user++) {
+        if((*user)->authenticate(username, password)) {
+            return (User*) *user;
+        }
+    }
+    WrongUsernameOrPasswordException ex;
+    throw ex;
+}
+void User::signup(vector<AbstractUser*> *users, string username, string password){
+    for (auto user = users->begin(); user != users->end(); user++) {
+        if ((*user)->username == username) {
+            UserAlreadyExistsException ex;
+            throw ex;
+        }
+    }
+    //Create user and add it to vector
+    users->push_back(new User(username, password, UserType::MEMBER));
+}
--- User.h
+++ User.h
@@ -5,10 +5,24 @@
 #ifndef STACKOVERFLOW_IN_CPP1_USER_H
 #define STACKOVERFLOW_IN_CPP1_USER_H
 
+#include "AbstractUser.h"
 
-class User {
+class User : public AbstractUser {
+public:
+    User(string username, string password, UserType type);
+public:
+    void set_password(string password);
+    bool check_password(string password);
 
+public:
+    bool authenticate(string username, string password);
+    void deleteAccount(vector<AbstractUser*> *users);
+
+public:
+    static User* login(vector<AbstractUser*> *users, string username, string password);
+    static void signup(vector<AbstractUser*> *users, string username, string password);
+private:
+    const string salt = "E1F53135E559C253";
 };
-
 
 #endif //STACKOVERFLOW_IN_CPP1_USER_H
