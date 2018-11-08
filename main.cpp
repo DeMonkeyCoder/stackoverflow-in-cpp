172d10b05989a71f6664de9500f13eb9234ea4c1
spsina <snparvizi75@gmail.com>
2018-11-08 13:37:32 +0330

    menu written using utils and no data base

--- main.cpp
+++ main.cpp
@@ -1,22 +1,12 @@
 #include <iostream>
-#include <vector>
-#include <functional>
-#include <sstream>
-#include <unistd.h>
-#include "myHash.h"
-#include <string>
-#include <unistd.h>
-#include "md5.h"
-#include <stdlib.h>
 #include <string>
 #include "AbstractUser.h"
 #include "Exceptions.h"
 #include "User.h"
-
+#include "AppDatabase.h"
+#include "Utils.h"
 using namespace std;
 using namespace Exceptions;
-
-
 
 
 enum MenuState {
@@ -25,27 +15,11 @@ enum MenuState {
     END
 };
 
-
-class AppDatabase { //Just holds runtime data. doesn't save anything
-public:
-    vector<AbstractUser*> appUsers;
-
-    AppDatabase() { //Load initial data
-        appUsers.push_back(new User("admin",
-                                    "admin" /* password is unsafe! for test only */,
-                                    UserType::ADMIN)
-        );
-    }
-    
-};
-
-
 int main() {
     User * loggedInUser = nullptr;
-    AppDatabase appDatabase;
     MenuState menuState = MenuState::START;
     string last_message = "";
-    //User users(&appDatabase.appUsers);
+
     char choice;
     while(menuState != MenuState::END) {
         system("clear");
@@ -58,15 +32,8 @@ int main() {
                 cin >> choice;
                 switch (choice) {
                     case '1': { // login
-                        string username;
-                        cout << "Enter Username: ";
-                        cin >> username;
-                        //char *pass = getpass("Enter password: ");
-                        string password;
-                        cout << "Enter Password: ";
-                        cin >> password;
                         try {
-                            loggedInUser = User::login(&appDatabase.appUsers, username, password);
+                            loggedInUser = Utils::loginUser();
                             menuState = MenuState::LOGGED_IN;
                         } catch (WrongUsernameOrPasswordException e) {
                             last_message = e.what();
@@ -75,15 +42,8 @@ int main() {
                         break;
                     }
                     case '2': { // signup
-                        string username;
-                        cout << "Enter Username: ";
-                        cin >> username;
-                        //char *pass = getpass("Enter password: ");
-                        string password;
-                        cout << "Enter Password: ";
-                        cin >> password;
                         try {
-                            User::signup(&appDatabase.appUsers, username, password);
+                            Utils::signUp();
                             last_message = "\n\nUser signed up!\n\n";
                         } catch (UserAlreadyExistsException e) {
                             last_message = e.what();
@@ -107,7 +67,7 @@ int main() {
                 switch (choice) {
                     case 'd': {
                         try {
-                            loggedInUser->deleteAccount(&appDatabase.appUsers);
+                            loggedInUser->deleteAccount();
                             cout << "\n\nAccount successfully deleted\n\n";
                             loggedInUser = nullptr;
                             menuState = MenuState::START;
