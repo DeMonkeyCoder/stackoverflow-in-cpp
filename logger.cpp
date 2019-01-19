0a08786fc5d0975b7bd40c72ff89ef6d7fcfcd44
s_arme <s.arme@ya.ru>
2019-01-20 01:14:15 +0330

    Add logger class

---
+++ logger.cpp
@@ -0,0 +1,5 @@
+//
+// Created by SARM on 20/01/2019.
+//
+
+#include "logger.h"
---
+++ logger.h
@@ -0,0 +1,14 @@
+//
+// Created by SARM on 20/01/2019.
+//
+
+#ifndef STACKOVERFLOW_IN_CPP_LOGGER_H
+#define STACKOVERFLOW_IN_CPP_LOGGER_H
+
+
+class logger {
+
+};
+
+
+#endif //STACKOVERFLOW_IN_CPP_LOGGER_H
